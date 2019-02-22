df7b7760093ce38b29dcea7e767a9f92f33b8824
Huoran <lihuoran@pku.edu.cn>
2019-02-22 15:09:05 +0800

    modify WeeklyContest/124/number-of-squareful-arrays.py

--- WeeklyContest/124/number-of-squareful-arrays.py
+++ WeeklyContest/124/number-of-squareful-arrays.py
@@ -4,19 +4,19 @@ class Solution:
             self.ans += 1
             return
 
-        for i in range(self.n):
-            if self.mark[i]:
-                continue
-            cur = self.a[i]
-            if i != 0 and self.mark[i - 1] == False and self.a[i] == self.a[i - 1]:
-                continue
-            if x != 0 and (cur + self.rec[x - 1]) not in self.square:
-                continue
+        last = None
+        for i, val in enumerate(self.a):
+            if self.usable[i]:
+                if val == last:
+                    continue
+                if x != 0 and (val + self.rec[x - 1]) not in self.square:
+                    continue
 
-            self.mark[i] = True
-            self.rec[x] = cur
-            self.dfs(x + 1)
-            self.mark[i] = False
+                self.usable[i] = False
+                self.rec[x] = val
+                self.dfs(x + 1)
+                self.usable[i] = True
+                last = val
 
     def numSquarefulPerms(self, A: 'List[int]') -> 'int':
         self.square = set([i ** 2 for i in range(32000)])
@@ -24,7 +24,8 @@ class Solution:
         self.a = sorted(A)
         self.n = len(self.a)
 
-        self.mark = [False for _ in range(self.n)]
-        self.rec = [None for _ in range(self.n)]
+        self.usable = [True] * self.n
+        self.rec = [None] * self.n
         self.dfs(0)
         return self.ans
+
